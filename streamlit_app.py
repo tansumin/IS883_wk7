5f40cac4af5cfbbdbfc5b868f866101c03393d03
Su Min <156374931+tansumin@users.noreply.github.com>
2024-10-15 16:28:47 -0400

    Updated secret key

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 import os
 
 ### Load your API Key
-my_secret_key = userdata.get('MyOpenAIKey')
+my_secret_key = st.secrets['MyOpenAIKey']
 os.environ["OPENAI_API_KEY"] = my_secret_key
 
 
