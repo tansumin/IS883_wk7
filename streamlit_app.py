9c921e379fca34325dd758045f11891d10a5d3fe
Su Min <156374931+tansumin@users.noreply.github.com>
2024-10-15 16:54:15 -0400

    Updated user content

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,14 +22,12 @@ response = client.chat.completions.create(
   model="gpt-4o-mini",
   messages=[
     {"role": "system", "content": "Complete the following prefix"},
-    {"role": "user", "content": "Barcelona is a"}
+    {"role": "user", "content": prompt}
   ],
-  n=10,
-  max_tokens=20
 )
 
 
 ### Display
-for i in range(10):
-  st.write(response.choices[i].message.content)   #cannot "print" in a webapp; have to code to show on UI
+st.write(
+    response.choices[0].message.content)   #cannot "print" in a webapp; have to code to show on UI
 
