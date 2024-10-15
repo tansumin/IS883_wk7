ae35fe4039b50db224c96766a0f6f59d2dab4d20
Su Min <156374931+tansumin@users.noreply.github.com>
2024-10-15 16:23:33 -0400

    Starting to build in OpenAI API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,28 @@
 import streamlit as st
+from openai import OpenAI
+import os
+
+### Load your API Key
+my_secret_key = userdata.get('MyOpenAIKey')
+os.environ["OPENAI_API_KEY"] = my_secret_key
+
+
+### Request the answer to the question "Damascus is a"
+client = OpenAI()
+response = client.chat.completions.create(
+  model="gpt-4o-mini",
+  messages=[
+    {"role": "system", "content": "Complete the following prefix"},
+    {"role": "user", "content": "Damascus is a"}
+  ],
+  seed = BUID,
+  n=10,
+  max_tokens=20
+)
+
+for i in range(10):
+  st.write(response.choices[i].message.content)   #cannot "print" in a webapp; have to code to show on UI
+
 
 st.title("🎈 My new app")
 st.write(
