7441740c1bb7ff7bea10e31c47ee98bddaf3d3ed
Su Min <156374931+tansumin@users.noreply.github.com>
2024-10-21 09:55:02 -0400

    Update streamlit_app.py to include prompt for response length

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,15 +10,17 @@ st.write(
 # Use a pipeline as a high-level helper
 from transformers import pipeline
 prompt = st.text_input("What is your prompt today?", "Damascus is")
+number_of_tokens = st.text_input("What is the number of tokens to be used?", "20")
 
+# Convert the number of tokens input to an integer
+number_of_tokens = int(number_of_tokens)
 
 ### Create a GPT2 generator pipeline
 generator = pipeline('text-generation', model='gpt2')
 
 ### Use the pipeline
-response = generator(prompt)
+response = generator(prompt, max_length= number_of_tokens)
 
 ### Display
-st.write(
-    response.choices[0].message.content)   #cannot "print" in a webapp; have to code to show on UI
+st.write(response[0].['generated_text'])   #cannot "print" in a webapp; have to code to show on UI
 
