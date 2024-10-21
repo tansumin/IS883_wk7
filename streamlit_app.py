647b1e6582ab438ca3840fe7c29cebe56c174fe6
Su Min <156374931+tansumin@users.noreply.github.com>
2024-10-21 10:28:33 -0400

    Update streamlit_app.py correct typo error for predictable

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,19 +12,19 @@ from transformers import pipeline
 prompt = st.text_input("What is your prompt today?", "Damascus is")
 number_of_tokens = st.text_input("What is the number of tokens to be used?", "20")
 creative_temperature = st.text_input("How creative do you want your responses? (Pick a value between 0 and 1)", "0.9")
-predictive_temperature = st.text_input("How predictive do you want your responses? (Pick a value between 0 and 1)", "0.2")
+predictable_temperature = st.text_input("How predictable do you want your responses? (Pick a value between 0 and 1)", "0.2")
 
 # Convert the number of tokens input to an integer
 number_of_tokens = int(number_of_tokens)
 creative_temperature = float(creative_temperature)
-predictive_temperature = float(predictive_temperature)
+predictable_temperature = float(predictive_temperature)
 
 ### Create a GPT2 generator pipeline
 generator = pipeline('text-generation', model='gpt2')
 
 ### Use the pipeline
 creative_response = generator(prompt, max_length= number_of_tokens, temperature= creative_temperature)
-predictive_response = generator(prompt, max_length= number_of_tokens, temperature= predictive_temperature)
+predictable_response = generator(prompt, max_length= number_of_tokens, temperature= predictable_temperature)
 
 ### Display
 st.write("Creative Response:")
